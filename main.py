44810422a512a08f90c68e4c74f74131e675c38c
Daniel Huynh <danielhuynh97@gmail.com>
2021-01-23 18:08:36 -0800

    added the excel functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import re
 from xlwt import Workbook
-from openpyxl.reader.excel import load_workbook
+from openpyxl import Workbook
 
 # Enter Local Path to Driver Chrome For Browser Support
 PATH = "/Users/daniel/code/scraper/chromedriver"
@@ -9,10 +9,16 @@ PATH = "/Users/daniel/code/scraper/chromedriver"
 # Instantiate Chrome Window
 driver = webdriver.Chrome(PATH)
 
+# Create New Workbook and open the current active window
+workbook = Workbook()
+sheet = workbook.active
+
+
+
 # Open Text File of Website List
-with open('websites.txt') as f:
+with open('websites.txt') as websiteSpreadsheet:
 	
-	for line in f: # Iterate through all lines
+	for line in websiteSpreadsheet: # Iterate through all lines
 		
 		# Grab Website URL and Set As Source
 		driver.get(line)
@@ -21,4 +27,11 @@ with open('websites.txt') as f:
 		# ReGEX find all email matches within HTML of website
 		FoundEmails = re.findall(r'[\w\.-]+@[\w\.-]+', LoadedPageSource)
 		for email in FoundEmails:
+			print(sheet.cell_value(email, 0))
+			sheet["B1"] = email
 			print(email) # Print the Email to Terminal to Show List
+	
+	# Save and Close Excel Workbook When Finished Executing 
+    workbook.Save()
+    workbook.Close()
+    sheet.Quit()
